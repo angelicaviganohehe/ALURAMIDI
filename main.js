5ff34d1dad42860ad0ed953fc8090178a4821fbc
angelicaviganohehe <106531341+angelicaviganohehe@users.noreply.github.com>
2023-09-08 10:59:13 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 alert('Olá, mundo');
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSomClap(){
+    document.querySelector('#som_tecla_clap').play();
     }
+    document.querySelector('.tecla_clap').onclick = tocaSomClap;
