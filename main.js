8393748491ab599b75a0f0921cef63d163112648
angelicaviganohehe <106531341+angelicaviganohehe@users.noreply.github.com>
2023-09-22 11:00:49 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ function tocaSomPom(){
     document.querySelectorAll('.tecla');
 const listaDeTeclas = document.querySelectorAll('.tecla');
 listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+//Estrutura de repetição - Enquanto
+while(contador <9){
+    listaDeTeclas[0].onclick = tocaSomPom;
+    contador = contador + 1;
+    console.log(contador);
 
-while (condition) {
-    
 }
