afcff1c20364eaafc39881136825f2edaf8e2c05
angelicaviganohehe <106531341+angelicaviganohehe@users.noreply.github.com>
2023-10-04 09:52:27 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,9 +6,16 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 listaDeTeclas[0].onclick = tocaSomPom;
 let contador = 0;
 //Estrutura de repetição - Enquanto
-while(contador <9){
-    listaDeTeclas[0].onclick = tocaSomPom;
+while(contador < listaDeTeclas.length){
+    listaDeTeclas[contador].onclick = tocaSom('#som_tecla_toim');
     contador = contador + 1;
     console.log(contador);
+    listaDeTeclas[contador]= tocaSomPom;}
+    function tocaSom(idElementoAudio){
+        document.querySelector(idElementoAudio).play();
 
-}
+    }
+    listaDeTeclas[contador].onclick = function () {
+        tocaSom('#som_tecla_splash');
+    };
+
