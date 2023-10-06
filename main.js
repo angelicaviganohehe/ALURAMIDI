8f4ad7c77062bc1ceb61a007ac092675f65fbbba
angelicaviganohehe <106531341+angelicaviganohehe@users.noreply.github.com>
2023-10-06 11:00:52 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSomPom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
     }
-    document.querySelectorAll('.tecla');
-const listaDeTeclas = document.querySelectorAll('.tecla');
-listaDeTeclas[0].onclick = tocaSomPom;
-let contador = 0;
-//Estrutura de repetição - Enquanto
-while(contador < listaDeTeclas.length){
-    listaDeTeclas[contador].onclick = tocaSom('#som_tecla_toim');
-    contador = contador + 1;
-    console.log(contador);
-    listaDeTeclas[contador]= tocaSomPom;}
-    function tocaSom(idElementoAudio){
-        document.querySelector(idElementoAudio).play();
 
-    }
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//Estrutura de repetição - Enquanto
+for(let contador=0;contador < listaDeTeclas.length;contador++){
+    const tecla =listaDeTeclas [contador];
+    const instrumento=tecla.classList[1];
+    const idAudio = '#som_ ${instrumento}';
+    
+
+    listaDeTeclas[contador].onclick = tocaSom('#som_tecla_toim');
+    listaDeTeclas[contador]= tocaSomPom;}
+    
+
     listaDeTeclas[contador].onclick = function () {
         tocaSom('#som_tecla_splash');
     };
+    listaDeTeclas[2].classList.add('ativa')
 
