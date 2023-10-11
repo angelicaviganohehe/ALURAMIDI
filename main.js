1b50e7b5752707917f05ece95478bbd729493565
angelicaviganohehe <106531341+angelicaviganohehe@users.noreply.github.com>
2023-10-11 09:52:41 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,19 +4,29 @@ function tocaSomPom(idElementoAudio){
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-//Estrutura de repetição - Enquanto
-for(let contador=0;contador < listaDeTeclas.length;contador++){
+//para
+for(let contador=0; contador < listaDeTeclas.length; contador++){
     const tecla =listaDeTeclas [contador];
-    const instrumento=tecla.classList[1];
-    const idAudio = '#som_ ${instrumento}';
-    
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
 
-    listaDeTeclas[contador].onclick = tocaSom('#som_tecla_toim');
-    listaDeTeclas[contador]= tocaSomPom;}
     
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
 
-    listaDeTeclas[contador].onclick = function () {
-        tocaSom('#som_tecla_splash');
-    };
-    listaDeTeclas[2].classList.add('ativa')
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento.code == 'Space')
+
+        if (evento.code === 'Space'){
+    tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
+    }
+
+}
 
